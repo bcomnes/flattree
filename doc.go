0c9b925deb3aa3d42dab7f42619234d98458f5d4
Bret Comnes <bcomnes@gmail.com>
2018-02-18 11:07:40 -0800

    Godoc

--- doc.go
+++ doc.go
@@ -3,7 +3,21 @@
 // that can be found in the LICENSE file.
 
 /*
+Package flattree implements a series of functions to map a binary tree to a list.
 
- */
+You can represent a binary tree in a simple flat list using the following structure
 
+			3
+		1       5
+	0   2   4   6  ...
+
+This module exposes a series of functions to help you build and maintain this data structure.
+
+See below for a list of additional resources
+
+flat-tree for node: https://github.com/mafintosh/flat-tree
+print-flat-tree: https://github.com/mafintosh/print-flat-tree
+flat-tree for rust: https://github.com/mafintosh/flat-tree-rs
+
+*/
 package flattree
